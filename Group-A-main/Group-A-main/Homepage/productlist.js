9c3d941cf6acbd4d594abeeac83d8cb50214a6c9
dipeshrayg <dipesh.ray.g@gmail.com>
2025-08-05 11:57:53 +0100

    Delete Group-A-main/Group-A-main/Homepage directory

--- Group-A-main/Group-A-main/Homepage/productlist.js
+++
@@ -1,6 +0,0 @@
-// Optional interactivity (you can expand this further)
-document.querySelectorAll('.phone-item button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert("Purchase process coming soon.");
-  });
-});
